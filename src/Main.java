988c977d6abc7e060b7957b86443562556a7c930
MalikNegmatov <maliknegmatov@gmail.com>
2023-05-04 12:00:19 +0300

    Недоделанная первая

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,50 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+//import java.util.Arrays;
+//import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        // Задачи блока Строки
+        task1(false);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + 2*i);
+    public static void task1(boolean flag) {
+        System.out.println("\nЗадача № 1");
+        String inputName, lastName, firstName, middleName;
+
+        if (flag) inputName = stringFromConsole();
+        else inputName = "Ivanov Ivan Ivanovich";
+
+        char[] arrayInputName = inputName.toCharArray();
+        int i = 0;
+        char blank = ' ';
+        while (arrayInputName[i] != blank) {
+            lastName = lastName.
         }
+
+    }
+
+    public static String stringFromConsole() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите Ваше имя:");
+        return scanner.next();
+    }
+//    /**
+//     * Метод для генерации массива заданной длины и разряда значений
+//     *
+//     * @param arrayLength -- длина массива
+//     * @param segmentLength -- интервал генерируемых чисел
+//     * @param segmentDisplace -- смещение интервала на величину +rightBound
+//     * @return массив случайных чисел
+//     */
+//    public static int[] generateRandomArray(int arrayLength, int segmentLength, int segmentDisplace) {
+//        java.util.Random random = new java.util.Random();
+//        int[] arr   = new int[arrayLength];
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = random.nextInt(segmentLength)+segmentDisplace;
+//        }
+//        return arr;
     }
 }
