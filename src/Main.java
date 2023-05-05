85604526adaaede03849e767821a8b8efccea628
MalikNegmatov <maliknegmatov@gmail.com>
2023-05-05 15:24:50 +0300

    Первая доп задача

--- src/Main.java
+++ src/Main.java
@@ -6,34 +6,54 @@ public class Main {
 
     public static void main(String[] args) {
         // Задачи блока Строки
-        task1(false);
-        //task1(true);
+        String strOutput, name = "Ivanov Ivan Ivanovich";
+        System.out.println("\nЗадача № 1");
+        strOutput = task1(name);
+        System.out.println(strOutput);
 
+        task2(name);
+
+        name = "Иванов Семён Семёнович";
+        task3(name);
+
+        // Дополнительные Задачи блока Строки
+        task4(name);
     }
 
-    public static void task1(boolean flag) {
-        System.out.println("\nЗадача № 1");
-        String inputName, lastName, firstName, middleName;
+    public static String task1(String inputName) {
+        String lastName, firstName, middleName, blank = " ";
 
-        if (flag) {
-            inputName = stringFromConsole();
-        }
-        else {
-            inputName = "Ivanov Ivan Ivanovich";
-        }
-
-        String[] names = inputName.split(" ");
+        String[] names = inputName.split(blank);
         lastName = names[0];
         firstName = names[1];
         middleName = names[2];
 
         StringBuilder fullName = new StringBuilder(lastName);
-        fullName.append(" " + firstName);
-        fullName.append(" " + middleName);
+        fullName.append(blank + firstName);
+        fullName.append(blank + middleName);
 
-        System.out.println(fullName);
+        return fullName.toString();
+    }
+    public static void task2(String fullName) {
+        System.out.println("\nЗадача № 2");
+
+        fullName=fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
+    }
+    public static void task3(String fullName) {
+        System.out.println("\nЗадача № 3");
+        fullName=fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника — " + fullName);
     }
 
+    public static void task4(String fullName) {
+        System.out.println("\nДоп. Задача № 4");
+        System.out.println(fullName);
+        int k = fullName.indexOf(" ");
+        System.out.println(k);
+        k = fullName.lastIndexOf(" ");
+        System.out.println(k);
+    }
     public static String stringFromConsole() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите Ваше имя:");
